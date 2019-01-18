7abac2bf132b1a6f22f90aee3beb6473ba763dc8
songshuran <songshuran@shouqianba.com>
2019-01-18 23:59:27 +0800

    ignore

---
+++ src/main/java/com/ooqiu/gaming/service/admin/api/ChannelService.java
@@ -0,0 +1,11 @@
+package com.ooqiu.gaming.service.admin.api;
+
+import com.ooqiu.gaming.server.domain.Channel;
+
+import java.util.List;
+
+public interface ChannelService {
+
+    public List<Channel> selectAll();
+
+}
